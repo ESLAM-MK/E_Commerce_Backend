507effaceaf5694ea886fd523bd100aca786c119
Eslam Medhat Kamel <medhateslam886@gmail.com>
2026-07-14 18:53:01 +0300

    updates in auth router

--- Routes/authRouter.js
+++ Routes/authRouter.js
@@ -1,9 +1,11 @@
 import express from "express"
-import { adminRegister, buyerRegister, employeeRegister, userLogin, userLogout } from "../controllers/authController.js"
+import { adminRegister, buyerRegister, employeeRegister, refreshToken, userLogin, userLogout } from "../controllers/authController.js"
+import { generateRefreshToken } from "../utils/generateTokens.js"
 const router = express.Router()
 router.post("/buyer-register",buyerRegister)
 router.post("/employee-register",employeeRegister)
 router.post("/admin-register",adminRegister)
 router.post("/login",userLogin)
 router.post("/logout",userLogout)
+router.post("/refresh-Token",refreshToken)
 export default router
